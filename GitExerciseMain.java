878b26f9b2d842e5cc03dec60b4079032dbd0670
MaltheGram <malthegram22@gmail.com>
2021-04-08 11:11:56 +0200

    Library main added to GitExerciseMain

--- GitExerciseMain.java
+++ GitExerciseMain.java
@@ -1,7 +1,27 @@
+import java.util.ArrayList;
+
 public class GitExerciseMain {
     public static void main(String[] args) {
         StringSort sort = new StringSort();
         sort.runStringSort();
+        System.out.println();
+
+        // Library Assignment
+        Library.printProblemDescription();
+
+        Book MacMasterRaceVolumeOne = new Book(252510, 2019, "MacMasterRaceVolumeOne");
+        Book MacMasterRaceVolumeTwo = new Book(252511, 2020, "MacMasterRaceVolumeTwo");
+        Book MacMasterRaceVolumeThree = new Book(252512, 2021, "MacMasterRaceVolumeThree");
+        Book MacMasterRaceVolumeFour = new Book(252513, 2021, "MacMasterRaceVolumeFour");
+        ArrayList<Book> bookList = new ArrayList<>();
+
+        bookList.add(MacMasterRaceVolumeOne);
+        bookList.add(MacMasterRaceVolumeTwo);
+        bookList.add(MacMasterRaceVolumeThree);
+
+        System.out.println(Library.isISBNMatching(MacMasterRaceVolumeOne,bookList));
+
+
     }
 }
 
