045ae62c0865ea2946a877df0168f243f0e718e8
Mark Friis Larsen <marklarsen96@gmail.com>
2021-04-08 11:31:57 +0200

    mark final final update

--- Search.java
+++ Search.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Search {
+    //made by Mark, the god among men.
     private String assignment = "Skriv en metode, der som parametre modtager et array af typen String samt en String." +
             "Metoden skal undersøge om strengen findes i arrayet. Metoden skal returnere indekspositionen, " +
             "hvis strengen findes i arrayet og–1hvis strengen ikke findes i arrayet.Lav en alternativ fejlhåndtering, " +
